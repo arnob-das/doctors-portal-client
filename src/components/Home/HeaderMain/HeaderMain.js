f4a1d49617fda8278d32a5dd1bf4cf01a1f7be15
arnob-das <contact.arnobdas@gmail.com>
2021-11-30 02:59:17 +0600

    header main done

---
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Chair from "../../../images/chair.png";
+import './HeaderMain.css'
+
+const HeaderMain = () => {
+  return (
+    <main style={{ height: "600px" }} className="row d-flex align-items-center">
+      <div className="col-md-4 offset-md-1">
+        <h1 style={{ color: "#3a4246" }}>
+          Your New Smile <br />
+          Starts Here
+        </h1>
+        <p className="text-secondary">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis
+          officiis harum illum eius id nihil?
+        </p>
+        <button className="btn btn-primary appointment-button">Get Appointment</button>
+      </div>
+      <div className="col-md-6">
+        <img src={Chair} alt="Chair" className="img-fluid" />
+      </div>
+    </main>
+  );
+};
+
+export default HeaderMain;
---
+++ src/components/Home/Navbar/Navbar.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link ms-5 active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link ms-5 active" aria-current="page" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link ms-5 active" aria-current="page" href="#">
+                  Dental Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link ms-5 text-white active" aria-current="page" href="#">
+                  Reviews
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link ms-5 text-white active" aria-current="page" href="#">
+                  Blogs
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link ms-5 text-white active" aria-current="page" href="#">
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
